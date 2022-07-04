e8faecefce60b60ce3f5f2f741128145f2596772
Tersaramor <nestulej00@gmail.com>
2022-07-04 05:18:57 +0300

    initial 2:18

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from pydantic import BaseModel
 from random import randint
 
@@ -24,19 +24,57 @@ def get_posts():
     return my_posts
 
 
+def find_post(post_id: int):
+    for post in my_posts:
+        if post['id'] == post_id:
+            return post
+
+
+def find_post_id(post_id: int):
+    for i, post in enumerate(my_posts):
+        if post['id'] == post_id:
+            return i
+
+
 @app.get("/posts/{post_id}")
-def get_post(post_id: int):
-    try:
-        [data] = list(filter(lambda x: x['id'] == post_id, my_posts))
-    except ValueError:
-        data = "Post not found"
+def get_post(post_id: int,  response: Response):
+    data = find_post(post_id)
+
+    if data is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {post_id} not found")
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {"message": f'post with id {post_id} not found'}
 
     return {"data": data}
 
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
-    data = post.dict()
-    data['id'] = randint(1, 1000000000)
-    my_posts.append(data)
-    return {"data": data}
+    post_dict = post.dict()
+    post_dict['id'] = randint(1, 1000000000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
+
+
+@app.delete("/posts/{post_id}")
+def delete_post(post_id: int):
+    index = find_post_id(post_id)
+
+    if index is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {post_id} not found")
+
+    del my_posts[index]
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.put("/posts/{post_id}", status_code=status.HTTP_200_OK)
+def create_posts(post_id: int, post: Post):
+    index = find_post_id(post_id)
+
+    if index is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {post_id} not found")
+
+    post_dict = post.dict()
+    post_dict['id'] = post_id
+    my_posts[index] = post_dict
+    return {"data": post_dict}
